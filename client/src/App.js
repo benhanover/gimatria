83b05a712ed0236e540bf78d1b622a47e9a65fa8
Ben <benhanover4@gmail.com>
2023-12-17 15:40:44 +0200

    basic home page structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-// import Test from "./components/Test"
-import Bla from "./components/Bla"
+import React, { useState } from 'react';
+import './index.css';
+import HomePage from './components/HomePage';
 
 function App() {
-  return(
-    <>
-      <Test />
-      <Bla />
-    </>
-  )
+  const [words, setWords] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <HomePage
+        words={words}
+        setWords={setWords}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import Word from './Word';
+
+function HomePage({ words, setWords, inputValue, setInputValue }) {
+  useEffect(() => {
+    // Make a GET request to the API endpoint
+    axios
+      .get('http://localhost:5000/api/data')
+      .then((response) => {
+        // Set the data in the state
+        setWords(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    // api call
+    console.log(inputValue);
+  }, [inputValue]);
+
+  return (
+    <div className="text-center bg-gray-200 p-8">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        className="border border-gray-300 p-4 mb-8 text-lg"
+        placeholder="Type something..."
+      />
+      {words ? (
+        <div className="grid grid-cols-2 gap-4">
+          {words.map((word, index) => (
+            <Word key={index} word={word} />
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default HomePage;
