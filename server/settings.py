cd5f62f1f220694e424935ced0d44fedc11dff68
nofatigue <nofatigua@gmail.com>
2023-12-20 14:44:00 +0200

    Added some explanations

--- server/settings.py
+++ server/settings.py
@@ -1,5 +1,10 @@
 from decouple import config
 import os
 
-# Define your configuration settings
+"""
+Expose settings from these sources:
+    1. environment variable
+    2. .env file somewhere in file path
+"""
+
 MONGO_URL = config("MONGO_URL")
